06b60591e4fa612a727142d05b900014e6c912c0
rkalil <rkalil@winsupply.com>
2025-05-11 19:10:47 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+#This is an python project that contains a pipeline solution
+#to monitor prices of a website called Mercado Livre and
+#generates a dashboard by using Scrapy, Pandas, Streamlit and SQL to achieve its purpose.
+
+#What is a ETL?
+#R: t's an abreviation for Extract, Transform and Load, being one of the principal methods for web scrapping.
+
